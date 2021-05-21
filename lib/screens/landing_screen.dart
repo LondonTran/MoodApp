f4f31636fcbfec3d4c5c2686419e1097ffe2a485
LondonTran <london.q.tran@gmail.com>
2021-05-20 23:48:43 -0400

    1. Added friends list listview in landing_screen.dart

--- lib/screens/landing_screen.dart
+++ lib/screens/landing_screen.dart
@@ -1,10 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 
-final _firestore = FirebaseFirestore.instance;
-User loggedInUser;
+User currentUser;
+FirebaseAuth auth = FirebaseAuth.instance;
 
 class LandingScreen extends StatefulWidget {
   static const String id = 'landing_screen';
@@ -20,21 +19,18 @@ class _LandingScreenState extends State<LandingScreen> {
   void initState() {
     super.initState();
     getCurrentUser();
-    getUsersFriends();
   }
 
   void getCurrentUser() async {
     try {
       final user = await _auth.currentUser;
       if (user != null) {
-        loggedInUser = user;
+        currentUser = user;
       }
     } catch (e) {
       print(e);
     }
   }
-
-  void getUsersFriends() async {}
 
   @override
   Widget build(BuildContext context) {
@@ -78,6 +74,26 @@ class _LandingScreenState extends State<LandingScreen> {
                   ),
                 ),
               ),
+            ),
+            ListView(
+              padding: const EdgeInsets.all(8),
+              children: <Widget>[
+                Container(
+                  height: 50,
+                  color: Colors.amber[600],
+                  child: const Center(child: Text('Entry A')),
+                ),
+                Container(
+                  height: 50,
+                  color: Colors.amber[500],
+                  child: const Center(child: Text('Entry B')),
+                ),
+                Container(
+                  height: 50,
+                  color: Colors.amber[100],
+                  child: const Center(child: Text('Entry C')),
+                ),
+              ],
             ),
           ],
         ),
