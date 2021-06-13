2c7aec220c3cf2dcd6d12a42a0d9f26a5aa2a84f
LondonTran <london.q.tran@gmail.com>
2021-06-12 22:07:41 -0400

    1. Removed reorderableListView from friends_feed.dart due to issues with long press reordering. Replaced with Card.

--- lib/components/friends_feed.dart
+++ lib/components/friends_feed.dart
@@ -17,30 +17,20 @@ class _FriendsFeedState extends State<FriendsFeed> {
 
   @override
   Widget build(BuildContext context) {
-    final ColorScheme colorScheme = Theme.of(context).colorScheme;
-    final Color oddItemColor = colorScheme.primary.withOpacity(0.05);
-    final Color evenItemColor = colorScheme.primary.withOpacity(0.15);
-
-    return ReorderableListView(
-      padding: const EdgeInsets.symmetric(horizontal: 40),
-      children: <Widget>[
-        for (int index = 0; index < widget.friendsList.length; index++)
-          ListTile(
-            key: Key('$index'),
-            tileColor:
-                widget.friendsList[index].isOdd ? oddItemColor : evenItemColor,
-            title: Text('Item ${widget.friendsList[index]}'),
-          ),
-      ],
-      onReorder: (int oldIndex, int newIndex) {
-        setState(() {
-          if (oldIndex < newIndex) {
-            newIndex -= 1;
-          }
-          final int item = widget.friendsList.removeAt(oldIndex);
-          widget.friendsList.insert(newIndex, item);
-        });
-      },
+    return Padding(
+      padding: EdgeInsets.fromLTRB(0.0, 20.0, 0.0, 0),
+      child: Card(
+        color: Colors.grey,
+        // elevation: 5.0,
+        shadowColor: Colors.white,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 10.0),
+          child: Text('Hello'),
+        ),
+      ),
     );
   }
 }
